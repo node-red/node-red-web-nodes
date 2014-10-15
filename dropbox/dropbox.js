86182a386e063cb05690c0b329e4d2b2a432e4db
Mark Hindess <mark.hindess@gmail.com>
2014-10-15 14:10:27 +0100

    Make missing credential checking more consistent.

--- dropbox/dropbox.js
+++ dropbox/dropbox.js
@@ -95,24 +95,28 @@ module.exports = function(RED) {
         this.filename = n.filename || "";
         this.dropboxConfig = RED.nodes.getNode(n.dropbox);
         var credentials = this.dropboxConfig ? this.dropboxConfig.credentials : {};
+        if (!credentials.appkey || !credentials.appsecret ||
+            !credentials.accesstoken) {
+            this.warn("Missing dropbox credentials");
+            return;
+        }
+
         var node = this;
-        if (credentials.appkey && credentials.appsecret &&
-            credentials.accesstoken) {
-            var dropbox = new Dropbox.Client({
-                //uid: credentials.uid,
-                key: credentials.appkey,
-                secret: credentials.appsecret,
-                token: credentials.accesstoken,
-            });
-            node.on("input", function(msg) {
-                var filename = this.filename || msg.filename;
-                if (filename === "") {
-                    node.warn("No filename specified");
-                    return;
-                }
-                msg.filename = filename;
-                node.status({fill:"blue",shape:"dot",text:"downloading"});
-                dropbox.readFile(filename, function(err, data) {
+        var dropbox = new Dropbox.Client({
+            //uid: credentials.uid,
+            key: credentials.appkey,
+            secret: credentials.appsecret,
+            token: credentials.accesstoken,
+        });
+        node.on("input", function(msg) {
+            var filename = this.filename || msg.filename;
+            if (filename === "") {
+                node.warn("No filename specified");
+                return;
+            }
+            msg.filename = filename;
+            node.status({fill:"blue",shape:"dot",text:"downloading"});
+            dropbox.readFile(filename, function(err, data) {
                     if (err) {
                         node.warn("download failed " + err.toString());
                         delete msg.payload;
@@ -121,13 +125,10 @@ module.exports = function(RED) {
                         msg.payload = data;
                         delete msg.error;
                     }
-                    node.status({});
-                    node.send(msg);
-                });
+                node.status({});
+                node.send(msg);
             });
-        } else {
-            node.warn("Missing dropbox credentials");
-        }
+        });
     }
     RED.nodes.registerType("dropbox",DropboxQueryNode);
 
@@ -137,52 +138,43 @@ module.exports = function(RED) {
         this.localFilename = n.localFilename || "";
         this.dropboxConfig = RED.nodes.getNode(n.dropbox);
         var credentials = this.dropboxConfig ? this.dropboxConfig.credentials : {};
+        if (!credentials.appkey || !credentials.appsecret ||
+            !credentials.accesstoken) {
+            this.warn("Missing dropbox credentials");
+            return;
+        }
         var node = this;
-        if (credentials.appkey && credentials.appsecret &&
-            credentials.accesstoken) {
-            var dropbox = new Dropbox.Client({
-                //uid: credentials.uid,
-                key: credentials.appkey,
-                secret: credentials.appsecret,
-                token: credentials.accesstoken,
-            });
-            node.status({fill:"blue",shape:"dot",text:"checking credentials"});
-            dropbox.getAccountInfo(function (err) {
-                if (err) {
-                    node.error("Error verifying credentials: " + err);
-                    node.status({fill:"red",shape:"ring",text:"access denied"});
+        var dropbox = new Dropbox.Client({
+            //uid: credentials.uid,
+            key: credentials.appkey,
+            secret: credentials.appsecret,
+            token: credentials.accesstoken,
+        });
+        node.status({fill:"blue",shape:"dot",text:"checking credentials"});
+        dropbox.getAccountInfo(function (err) {
+            if (err) {
+                node.error("Error verifying credentials: " + err);
+                node.status({fill:"red",shape:"ring",text:"access denied"});
+                    return;
+            }
+            node.status({});
+            node.on("input", function(msg) {
+                var filename = this.filename || msg.filename;
+                if (filename === "") {
+                    node.warn("No filename specified");
                     return;
                 }
-                node.status({});
-                node.on("input", function(msg) {
-                    var filename = this.filename || msg.filename;
-                    if (filename === "") {
-                        node.warn("No filename specified");
-                        return;
-                    }
-                    var localFilename = this.localFilename || msg.localFilename;
-                    if (localFilename) {
-                        // TODO: use chunked upload for files larger than 150M
-                        node.status({fill:"blue",shape:"dot",text:"uploading"});
-                        fs.readFile(localFilename, function read(err, data) {
-                            if (err) {
-                                node.error(err.toString());
-                                node.status({fill:"red",shape:"ring",text:"failed"});
-                                return;
-                            }
+                var localFilename = this.localFilename || msg.localFilename;
+                if (localFilename) {
+                    // TODO: use chunked upload for files larger than 150M
+                    node.status({fill:"blue",shape:"dot",text:"uploading"});
+                    fs.readFile(localFilename, function read(err, data) {
+                        if (err) {
+                            node.error(err.toString());
+                            node.status({fill:"red",shape:"ring",text:"failed"});
+                            return;
+                        }
 
-                            dropbox.writeFile(filename, data, function(err) {
-                                if (err) {
-                                    node.error(err.toString());
-                                    node.status({fill:"red",shape:"ring",text:"failed"});
-                                    return;
-                                }
-                                node.status({});
-                            });
-                        });
-                    } else if (typeof msg.payload !== "undefined") {
-                        var data = RED.util.ensureBuffer(msg.payload);
-                        node.status({fill:"blue",shape:"dot",text:"uploading"});
                         dropbox.writeFile(filename, data, function(err) {
                             if (err) {
                                 node.error(err.toString());
@@ -191,12 +183,21 @@ module.exports = function(RED) {
                             }
                             node.status({});
                         });
-                    }
-                });
+                    });
+                } else if (typeof msg.payload !== "undefined") {
+                    var data = RED.util.ensureBuffer(msg.payload);
+                    node.status({fill:"blue",shape:"dot",text:"uploading"});
+                    dropbox.writeFile(filename, data, function(err) {
+                        if (err) {
+                            node.error(err.toString());
+                            node.status({fill:"red",shape:"ring",text:"failed"});
+                            return;
+                        }
+                        node.status({});
+                    });
+                }
             });
-        } else {
-            node.warn("Missing dropbox credentials");
-        }
+        });
     }
     RED.nodes.registerType("dropbox out",DropboxOutNode);
 };
