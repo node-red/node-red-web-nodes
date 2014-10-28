300ef9eaf47a8b62fa2b18a23be9d81bf1c98f27
Mark Hindess <mark.hindess@gmail.com>
2014-10-28 22:23:59 +0000

    Add dropbox output and query node good path tests.

--- test/dropbox/dropbox_spec.js
+++ test/dropbox/dropbox_spec.js
@@ -15,8 +15,16 @@
  **/
 
 var should = require("should");
+var sinon = require("sinon");
 var dropboxNode = require("../../dropbox/dropbox.js");
 var helper = require('../helper.js');
+var nock = helper.nock;
+
+// Force dropbox api server name to be api.dropbox.com
+var Dropbox = require('dropbox');
+Dropbox.Client.prototype._defaultMaxApiServer = function() {
+    return 0;
+};
 
 describe('dropbox nodes', function() {
 
@@ -28,14 +36,102 @@ describe('dropbox nodes', function() {
         helper.unload();
     });
 
-    describe('out node', function() {
-        it('can be loaded', function(done) {
+    describe("query node", function() {
+        if (!nock) return;
+        it('should fetch file', function(done) {
+            nock('https://api-content.dropbox.com:443')
+                .get('/1/files/auto/foo.txt')
+                .reply(200, "ABCD\n", {
+                    date: 'Tue, 28 Oct 2014 22:29:04 GMT',
+                    'content-type': 'text/plain; charset=utf-8',
+                    'content-length': '5',
+                    connection: 'keep-alive',
+                });
             helper.load(dropboxNode,
-                        [{id:"n1", type:"helper", wires:[["n2"]]},
-                         {id:"n2", type:"dropbox out"}], function() {
-                var n2 = helper.getNode("n2");
-                n2.should.have.property('id', 'n2');
-                done();
+                [{id:"inject", type: "helper", wires: [["dropbox"]]},
+                 {id:"dropbox-config", type: "dropbox-config"},
+                 {id:"dropbox", type:"dropbox",
+                  dropbox: "dropbox-config", wires: [["output"]] },
+                 {id:"output", type: "helper" },
+                ], {
+                    "dropbox-config": {
+                        "appkey":"FADE",
+                        "appsecret":"DEAD",
+                        "accesstoken":"BEEF"
+                    },
+                }, function() {
+                    var dropbox = helper.getNode("dropbox");
+                    dropbox.should.have.property('id', 'dropbox');
+                    var inject = helper.getNode("inject");
+                    inject.should.have.property('id', 'inject');
+                    var output = helper.getNode("output");
+                    output.should.have.property('id', 'output');
+                    output.on("input", function(msg) {
+                        msg.should.have.property('payload', "ABCD\n");
+                        msg.should.have.property('filename', 'foo.txt');
+                        done();
+                    });
+                    inject.send({
+                        filename: "foo.txt",
+                    });
+                });
+        });
+    });
+
+    describe('out node', function() {
+        if (!nock) return;
+        it('should upload msg.payload', function(done) {
+            nock('https://api.dropbox.com:443')
+                .get('/1/account/info')
+                .reply(200, {"referral_link":"https://example.com/foo","display_name":"Fred Bar","uid":5,"email_verified":true,"team":null,"quota_info":{"datastores":0,"shared":0,"quota":1024,"normal":512},"country":"GB","email":"junk@example.com"}, {
+                    date: 'Tue, 28 Oct 2014 21:53:52 GMT',
+                    'content-type': 'text/javascript',
+                    'transfer-encoding': 'chunked',
+                    connection: 'keep-alive',
+                  });
+            nock('https://api-content.dropbox.com:443')
+                .post('/1/files_put/auto/foo.txt', "ABCD\n")
+                .reply(200, {"revision":1,"bytes":5,"thumb_exists":false,"rev":"4","modified":"Wed, 15 Oct 2014 13:14:10 +0000","mime_type":"text/plain","path":"/foo.txt","is_dir":false,"size":"5 bytes","root":"app_folder","client_mtime":"Wed, 15 Oct 2014 13:14:10 +0000","icon":"page_white_text"}, {
+                    date: 'Tue, 28 Oct 2014 21:55:39 GMT',
+                    'content-type': 'text/javascript',
+                    'transfer-encoding': 'chunked',
+                    connection: 'keep-alive',
+                });
+            helper.load(dropboxNode,
+                [
+                    {id:"inject", type:"helper", wires:[["dropbox"]]},
+                    {id:"dropbox-config", type: "dropbox-config"},
+                    {id:"dropbox", type:"dropbox out",
+                     dropbox: "dropbox-config" },
+                ],
+                { "dropbox-config":
+                    {"appkey":"FADE","appsecret":"DEAD","accesstoken":"BEEF"}
+                }, function() {
+                    var dropbox = helper.getNode("dropbox");
+                    dropbox.should.have.property('id', 'dropbox');
+                    var inject = helper.getNode("inject");
+                    inject.should.have.property('id', 'inject');
+
+                     // wait for dropbox.on("input", ...) to be called
+                     var onFunction = dropbox.on;
+                     var onStub = sinon.stub(dropbox, 'on', function(event, cb) {
+                         var res = onFunction.apply(dropbox, arguments);
+                         onStub.restore();
+                         // stub status call to wait for successful upload
+                         var stub = sinon.stub(dropbox, 'status', function(status) {
+                             if (Object.getOwnPropertyNames(status).length === 0) {
+                                 stub.restore();
+                                 done();
+                             }
+                             return;
+                         });
+                         inject.send({
+                             filename: 'foo.txt',
+                             payload:  "ABCD\n",
+                         });
+                         return res;
+                     });
+
             });
         });
     });
