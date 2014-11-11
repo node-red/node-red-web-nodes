e72534bc46b80dad22d5511d151ff209eb804638
Mark Hindess <mark.hindess@gmail.com>
2014-11-11 15:14:21 +0000

    Google calendar node tests.

---
+++ test/google/calendar_spec.js
@@ -0,0 +1,318 @@
+/**
+ * Copyright 2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+var should = require("should");
+var sinon = require('sinon');
+var calendarNode = require("../../google/calendar.js");
+var googleNode = require("../../google/google.js");
+var helper = require('../helper.js');
+var nock = helper.nock;
+
+function ISOTimeString(offsetSeconds) {
+    if (!offsetSeconds) {
+     offsetSeconds = 0;
+    }
+    return (new Date((new Date()).getTime()+offsetSeconds*1000)).toISOString();
+}
+
+describe('google calendar nodes', function() {
+
+    before(function(done) {
+        helper.startServer(done);
+    });
+
+    afterEach(function() {
+        helper.unload();
+    });
+
+    describe('query node', function() {
+        if (!nock) { return; }
+        it('returns calendar entry', function(done) {
+            var oneHourAgo = ISOTimeString(-3600);
+            var oneHourFromNow = ISOTimeString(3600);
+            var twoHoursFromNow = ISOTimeString(7200);
+            nock('https://www.googleapis.com:443')
+                .get('/calendar/v3/users/me/calendarList')
+                .reply(200, {
+                    kind : "calendar#calendarList",
+                    items : [
+                        { id: "bob", summary: "Bob", primary: true },
+                        { id: "work", summary: "Work" },
+                        { id: "home", summary: "Home" }
+                    ]
+                }, {
+                    date: 'Tue, 11 Nov 2014 10:53:24 GMT',
+                    'content-type': 'application/json; charset=UTF-8'
+                })
+                .filteringPath(/timeMin=[^&]*/g, 'timeMin=now')
+                .get('/calendar/v3/calendars/bob/events?maxResults=10&orderBy=startTime&singleEvents=true&showDeleted=false&timeMin=now')
+                .reply(200, {
+                    kind: "calendar#events",
+                    items: [
+                        {
+                            creator: {
+                                email: "foo@example.com",
+                                self: true,
+                                displayName: "Bob Foo"
+                            },
+                            status: "confirmed",
+                            kind: "calendar#event",
+                            summary: "Coffee",
+                            attendees: [
+                                {
+                                    email: "foo@example.com",
+                                    responseStatus: "needsAction",
+                                    organizer: true,
+                                    self: true,
+                                    displayName: "Bob Foo"
+                                }
+                            ],
+                            start: {
+                                dateTime: oneHourAgo
+                            },
+                            end: {
+                                dateTime: oneHourFromNow
+                            }
+                        },
+                        {
+                            creator: {
+                                email: "foo@example.com",
+                                self: true,
+                                displayName: "Bob Foo"
+                            },
+                            status: "tentative",
+                            kind: "calendar#event",
+                            summary: "Meeting",
+                            attendees: [
+                                {
+                                    email: "foo@example.com",
+                                    responseStatus: "accepted",
+                                    organizer: true,
+                                    self: true,
+                                    displayName: "Bob Foo"
+                                }
+                            ],
+                            start: {
+                                dateTime: oneHourFromNow
+                            },
+                            end: {
+                                dateTime: twoHoursFromNow
+                            }
+                        }
+                    ]
+                }, {
+                    date: 'Tue, 11 Nov 2014 10:53:24 GMT',
+                    'content-type': 'application/json; charset=UTF-8'
+                });
+            helper.load([googleNode, calendarNode], [
+                {id:"google-config", type:"google-credentials",
+                    displayName: "Bob"},
+                {id:"calendar", type:"google calendar",
+                    google: "google-config", wires:[["output"]]},
+                {id:"output", type:"helper"}
+            ], {
+                "google-config": {
+                    clientId: "id",
+                    clientSecret: "secret",
+                    accessToken: "access",
+                    refreshToken: "refresh",
+                    expireTime: 1000+(new Date().getTime()/1000),
+                    displayName: "Bob"
+                },
+            }, function() {
+                var calendar = helper.getNode("calendar");
+                calendar.should.have.property('id', 'calendar');
+                var output = helper.getNode("output");
+                output.should.have.property('id', 'output');
+                output.on("input", function(msg) {
+                    msg.should.have.property('title', 'Meeting');
+                    done();
+                });
+
+                // wait for calendar.on("input", ...) to be called
+                var onFunction = calendar.on;
+                var onStub = sinon.stub(calendar, 'on', function() {
+                    var res = onFunction.apply(calendar, arguments);
+                    onStub.restore();
+                    calendar.emit('input', {}); // trigger poll
+                    return res;
+                });
+            });
+        });
+    });
+
+    describe('out node', function() {
+        if (!nock) { return; }
+        it('creates entry with quickAdd', function(done) {
+            nock('https://www.googleapis.com:443')
+                .get('/calendar/v3/users/me/calendarList')
+                .reply(200, {
+                    kind : "calendar#calendarList",
+                    items : [
+                        { id: "bob", summary: "Bob", primary: true },
+                        { id: "work", summary: "Work" },
+                        { id: "home", summary: "Home" }
+                    ]
+                }, {
+                    date: 'Tue, 11 Nov 2014 10:53:24 GMT',
+                    'content-type': 'application/json; charset=UTF-8'
+                })
+                .post('/calendar/v3/calendars/bob/events/quickAdd',
+                    "text=11%3A00%20coffee%20at%20Starbucks")
+                .reply(200, {
+                    creator: {
+                        email: "foo@example.com",
+                        self: true,
+                        displayName: "Bob Foo"
+                    },
+                    status: "confirmed",
+                    kind: "calendar#event",
+                    summary: "Coffee",
+                    location: "Starbucks",
+                    start: {
+                        dateTime: "2014-11-12T11:00:00Z"
+                    },
+                    end: {
+                        dateTime: "2014-11-12T12:00:00Z"
+                    }
+                }, {
+                    date: 'Tue, 12 Nov 2014 8:02:29 GMT',
+                    'content-type': 'application/json; charset=UTF-8'
+                });
+            helper.load([googleNode, calendarNode], [
+                {id:"google-config", type:"google-credentials",
+                    displayName: "Bob"},
+                {id:"calendar", type:"google calendar out",
+                    google: "google-config"}
+            ], {
+                "google-config": {
+                    clientId: "id",
+                    clientSecret: "secret",
+                    accessToken: "access",
+                    refreshToken: "refresh",
+                    expireTime: 1000+(new Date().getTime()/1000),
+                    displayName: "Bob"
+                },
+            }, function() {
+                var calendar = helper.getNode("calendar");
+                calendar.should.have.property('id', 'calendar');
+
+                var stub = sinon.stub(calendar, 'status', function(status) {
+                    if (Object.getOwnPropertyNames(status).length === 0) {
+                        stub.restore();
+                        done();
+                    }
+                    return;
+                });
+
+                // wait for calendar.on("input", ...) to be called
+                var onFunction = calendar.on;
+                var onStub = sinon.stub(calendar, 'on', function() {
+                    var res = onFunction.apply(calendar, arguments);
+                    onStub.restore();
+                    calendar.emit('input', {
+                        payload: "11:00 coffee at Starbucks"
+                    });
+                    return res;
+                });
+            });
+        });
+
+        it('creates entry with quickAdd', function(done) {
+            nock('https://www.googleapis.com:443')
+                .get('/calendar/v3/users/me/calendarList')
+                .reply(200, {
+                    kind : "calendar#calendarList",
+                    items : [
+                        { id: "bob", summary: "Bob", primary: true },
+                        { id: "work", summary: "Work" },
+                        { id: "home", summary: "Home" }
+                    ]
+                }, {
+                    date: 'Tue, 11 Nov 2014 10:53:24 GMT',
+                    'content-type': 'application/json; charset=UTF-8'
+                })
+                .post('/calendar/v3/calendars/bob/events', {
+                    start: { dateTime: "2014-11-12T11:00:00Z" },
+                    end: { dateTime: "2014-11-12T12:00:00Z" },
+                    location: "Starbucks",
+                    summary: "coffee"
+                })
+                .reply(200, {
+                    creator: {
+                        email: "foo@example.com",
+                        self: true,
+                        displayName: "Bob Foo"
+                    },
+                    status: "confirmed",
+                    kind: "calendar#event",
+                    summary: "Coffee",
+                    location: "Starbucks",
+                    start: {
+                        dateTime: "2014-11-12T11:00:00Z"
+                    },
+                    end: {
+                        dateTime: "2014-11-12T12:00:00Z"
+                    }
+                }, {
+                    date: 'Tue, 12 Nov 2014 8:02:29 GMT',
+                    'content-type': 'application/json; charset=UTF-8'
+                });
+            helper.load([googleNode, calendarNode], [
+                {id:"google-config", type:"google-credentials",
+                    displayName: "Bob"},
+                {id:"calendar", type:"google calendar out",
+                    google: "google-config"}
+            ], {
+                "google-config": {
+                    clientId: "id",
+                    clientSecret: "secret",
+                    accessToken: "access",
+                    refreshToken: "refresh",
+                    expireTime: 1000+(new Date().getTime()/1000),
+                    displayName: "Bob"
+                },
+            }, function() {
+                var calendar = helper.getNode("calendar");
+                calendar.should.have.property('id', 'calendar');
+
+                var stub = sinon.stub(calendar, 'status', function(status) {
+                    if (Object.getOwnPropertyNames(status).length === 0) {
+                        stub.restore();
+                        done();
+                    }
+                    return;
+                });
+
+                // wait for calendar.on("input", ...) to be called
+                var onFunction = calendar.on;
+                var onStub = sinon.stub(calendar, 'on', function() {
+                    var res = onFunction.apply(calendar, arguments);
+                    onStub.restore();
+                    calendar.emit('input', {
+                        payload: {
+                            start: { dateTime: "2014-11-12T11:00:00Z" },
+                            end: { dateTime: "2014-11-12T12:00:00Z" },
+                            location: "Starbucks",
+                            summary: "coffee"
+                        }
+                    });
+                    return res;
+                });
+            });
+        });
+    });
+});
