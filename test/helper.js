333ca2503920a3f535be16850048fd9365fbca53
Dave Conway-Jones <conway@uk.ibm.com>
2018-05-04 12:48:41 +0100

    retry

--- test/helper.js
+++ test/helper.js
@@ -1,25 +1,11 @@
-/**
- * Copyright 2014 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
 
 var path = require('path');
 
 //process.env.NODE_RED_HOME = process.env.NODE_RED_HOME || path.resolve(__dirname+"/../../node-red");
 //var helper = require(path.join(process.env.NODE_RED_HOME, 'test', 'nodes', 'helper.js'));
-var helper = require(path.resolve(__dirname + '/../node_modules/node-red/test/nodes/helper.js'));
-console.log("PATH",path.resolve(__dirname + '/../node_modules/node-red/test/nodes/helper.js'));
+// var helper = require(path.resolve(__dirname + '/../node_modules/node-red/test/nodes/helper.js'));
+//console.log("PATH",path.resolve(__dirname + '/../node_modules/node-red/test/nodes/helper.js'));
+var helper = require("node-red-node-test-helper");
 
 try {
     helper.nock = helper.nock || require("nock");
