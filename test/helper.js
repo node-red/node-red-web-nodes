b61b705ad373e73ef871fe5626cae4e1b6e86c03
Dave Conway-Jones <conway@uk.ibm.com>
2018-05-04 17:51:18 +0100

    getting there

--- test/helper.js
+++ test/helper.js
@@ -3,13 +3,11 @@ var path = require('path');
 
 //process.env.NODE_RED_HOME = process.env.NODE_RED_HOME || path.resolve(__dirname+"/../../node-red");
 //var helper = require(path.join(process.env.NODE_RED_HOME, 'test', 'nodes', 'helper.js'));
-// var helper = require(path.resolve(__dirname + '/../node_modules/node-red/test/nodes/helper.js'));
-//console.log("PATH",path.resolve(__dirname + '/../node_modules/node-red/test/nodes/helper.js'));
 var helper = require("node-red-node-test-helper");
 
 try {
     helper.nock = helper.nock || require("nock");
-} catch(er) {
+} catch(e) {
     helper.nock = null;
 }
 module.exports = helper;
