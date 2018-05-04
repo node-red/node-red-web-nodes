ec1b6c54c86c5d5a75ae1ff215a3175995af5771
Dave Conway-Jones <conway@uk.ibm.com>
2018-05-04 10:47:54 +0100

    try to fix helper

--- test/helper.js
+++ test/helper.js
@@ -18,7 +18,8 @@ var path = require('path');
 
 //process.env.NODE_RED_HOME = process.env.NODE_RED_HOME || path.resolve(__dirname+"/../../node-red");
 //var helper = require(path.join(process.env.NODE_RED_HOME, 'test', 'nodes', 'helper.js'));
-var helper = require(path.join('../node_modules/node-red', 'test', 'nodes', 'helper.js'));
+var helper = require(path.resolve(__dirname + '/../node_modules/node-red/test/nodes/helper.js'));
+console.log("PATH",path.resolve(__dirname + '/../node_modules/node-red/test/nodes/helper.js'));
 
 try {
     helper.nock = helper.nock || require("nock");
