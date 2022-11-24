eadd27feea3c46b7007e79220d0fbe715b96e89e
bartbutenaers <bart.butenaers@gmail.com>
2022-11-24 18:53:44 +0100

    Use refresh tokens for Dropbox (#301)
    * oath refresh token
    
    * Bump minimal dropbox version 10.32.0
    
    * missing refresh token
    
    * obsolete accesstoken code
    
    * Removed dot after url
    
    * remove refresh access token feature
    
    * text about token revoking
    
    * Bump minimatch version

--- dropbox/dropbox.js
+++ dropbox/dropbox.js
@@ -25,12 +25,14 @@ module.exports = function(RED) {
     function DropboxNode(n) {
         RED.nodes.createNode(this,n);
         this.dropbox = new Dropbox({
-            accessToken: this.credentials.accesstoken
+            clientId: this.credentials.clientid,
+            refreshToken: this.credentials.refreshtoken
         });
     }
     RED.nodes.registerType("dropbox-config",DropboxNode,{
         credentials: {
-            accesstoken: { type:"password" },
+            clientid: { type:"password" },
+            refreshtoken: { type:"password" }
         }
     });
 
@@ -39,8 +41,8 @@ module.exports = function(RED) {
 
         var dropboxConfig = RED.nodes.getNode(n.dropbox);
         var credentials = dropboxConfig ? dropboxConfig.credentials : {};
-        if (!credentials.accesstoken) {
-            this.warn(RED._("dropbox.warn.missing-credentials"));
+        if (!credentials.refreshtoken) {
+            this.warn(RED._("dropbox.warn.missing-refresh-token"));
             return;
         }
 
@@ -203,8 +205,8 @@ module.exports = function(RED) {
 
         var dropboxConfig = RED.nodes.getNode(n.dropbox);
         var credentials = dropboxConfig ? dropboxConfig.credentials : {};
-        if (!credentials.accesstoken) {
-            this.warn(RED._("dropbox.warn.missing-credentials"));
+        if (!credentials.refreshtoken) {
+            this.warn(RED._("dropbox.warn.missing-refresh-token"));
             return;
         }
         var node = this;
@@ -256,8 +258,8 @@ module.exports = function(RED) {
 
         var dropboxConfig = RED.nodes.getNode(n.dropbox);
         var credentials = dropboxConfig ? dropboxConfig.credentials : {};
-        if (!credentials.accesstoken) {
-            this.warn(RED._("dropbox.warn.missing-credentials"));
+        if (!credentials.refreshtoken) {
+            this.warn(RED._("dropbox.warn.missing-refresh-token"));
             return;
         }
         var node = this;
@@ -323,4 +325,96 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("dropbox out",DropboxOutNode);
+
+    var globalDropboxInstance = null;
+
+    RED.httpAdmin.get('/dropbox/generate_redirect_url', function(req, res) {
+        // Once the Dropbox authentication server has finished its authentication flow, it will send the results (incl. the refresh token) to our redirect url.
+        // Since the Dropbox instance (= the dropbox client) is executing the redirect, "localhost" will also be a valid host.
+        // Parts of the original will be reused, to make sure all path related settings.js adjustments are used here.
+        var redirectUrl = req.protocol + "://" + req.get('host') + req.originalUrl.replace("generate_redirect_url", "authenticate");
+        res.json({redirectUrl: redirectUrl});
+    });
+
+    RED.httpAdmin.get('/dropbox/generate_authentication_url', function(req, res) {
+        var options = {};
+
+        // When the config node has been deployed previously, it might contain deployed settings which need to be used
+        var dropboxNode = RED.nodes.getNode(req.query.nodeId);
+        if(dropboxNode) {
+            options.clientId = dropboxNode.credentials.clientid;
+        }
+        
+        // The config node's config screen might contain an undeployed clientId, which will override the client id from the deployed config node settings.
+        if (req.query.clientid && req.query.clientid !== "__PWRD__") {
+            options.clientId = req.query.clientid;
+        }
+        
+        if (!options.clientId) {
+            res.writeHead(500);
+            res.end(RED._("dropbox.error.no-app-key"));
+            return;
+        }
+
+        // Pass the redirect uri to our second 'authenticate' endpoint via the session state, because getAccessTokenFromCode needs the redirectUri we have been using here. 
+        var state = JSON.stringify({redirectUri: req.query.redirectUrl});
+
+        try {
+            // Create a new temporary Dropbox instance, because at this moment the config node (and its Dropbox instance) might not have been deployed yet.
+            var tempDropbox = new Dropbox(options);
+
+            // Get the authentication URL of this client app, to start an authentication flow on the Dropbox authentication server.
+            // The 'offline' argument is required to get a (long-lived) refresh token.  Because offline applications will access the API, without
+            // the manual intervention of an end user.  That refresh token can be used afterwards to obtain a new (short-lived) access token.
+            var authUrl = tempDropbox.auth.getAuthenticationUrl(req.query.redirectUrl, state, 'code', 'offline', null, 'none', true)
+            .then(function(authUrl) {
+                // In the next 'authenticate' endpoint, again a Dropbox instance will be needed.  Pass the current dropbox instance to the next endpoint, because
+                // (as a result of the getAuthenticationUrl call) it's 'codeVerifier' property will be filled in.  And that property is needed to call
+                // getAccessTokenFromCode in the next endpoint, to avoid having the user to go throught an entire Authorization Code Flow (with PKCE).
+                // Reusing the Dropbox instance seems more secure, compared to pass the codeVerifier via the session state to the next endpoint.
+                globalDropboxInstance = tempDropbox;
+    
+                // Redirect the request from the Node-RED flow editor to the Dropbox authentication server.
+                // The user should manually confirm (in the Dropbox authentication page) that this client app can be linked to the dropbox account.
+                // This starts the OAuth 2.0 authorization flow. This isn't an API call—it's the web page that lets the user sign in to Dropbox and authorize your app. 
+                // After the user decides whether or not to authorize your app, they will be redirected to the URI specified by redirect_uri.
+                res.json({authUrl: authUrl});
+            })
+            .catch(function(error) {
+                res.writeHead(500);
+                res.end(RED._("dropbox.error.auth-url-failed"));
+                return;
+            })
+        }
+        catch(err) {
+            res.writeHead(500);
+            res.end(err.message);            
+        }
+    });
+
+    // Dropbox will call this endpoint as soon as i authentication flow has ended
+    RED.httpAdmin.get('/dropbox/authenticate', function(req, res) {
+        var accessCode = req.query.code;
+        var state = JSON.parse(req.query.state);
+
+        // Ask Dropbox for an access token (and refresh token), based on the access code that we have received.
+        // Note that redirectUri won't be called again here, but the URL needs to be exactly the same as the one that has been called previously (in getAuthenticationUrl).
+        // Show the refresh token in the popup window, where it can be copied by the user and pasted into the config node's screen.
+        globalDropboxInstance.auth.getAccessTokenFromCode(state.redirectUri, accessCode)
+        .then(function(response) {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            
+            // The response contains two tokens (but only the refresh token is relevant for our use case):
+            // 1.- A (long-lived) refresh token which will never expire.
+            // 2.- A (short-lived) access token which will expire after 4 hours.  Note such tokens start with "sl.".
+            var html = "<html><head><title>" +  RED._("dropbox.title.auth-flow") + "</title></head><body><h1>" + RED._("dropbox.label.refreshtoken") + "</h1>" + response.result.refresh_token + "</body></html>";
+            res.end(html);
+            globalDropboxInstance = null;
+        })
+        .catch(function(error) {
+            res.writeHead(500);
+            res.end(err.message);
+        });
+    });
 };
